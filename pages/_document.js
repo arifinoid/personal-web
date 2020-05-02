338d1f178283e1a99ba2e016ce4a6418d8a75383
arifinoid <rohmadarifin007@gmail.com>
2020-05-03 00:57:12 +0700

    Fix flickered styled components

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,29 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const originalRenderPage = ctx.renderPage;
+    const sheet = new ServerStyleSheet();
 
     ctx.renderPage = () =>
       originalRenderPage({
-        enhanceApp: (App) => App,
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
         enhanceComponent: (Component) => Component,
       });
 
     const initialProps = await Document.getInitialProps(ctx);
 
-    return { ...initialProps };
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    };
   }
 
   render() {
